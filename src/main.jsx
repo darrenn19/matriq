1bbb99084fce96d00fe055ac190832db5010b7af
darrenn2020 <darrenn2020@gmail.com>
2025-11-07 22:18:39 -0500

    fix paths

--- src/main.jsx
+++ src/main.jsx
@@ -1,18 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { HashRouter, Routes, Route } from 'react-router-dom'
 import Dashboard from './pages/dashboard'
 //import TopicView from './pages/TopicView'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<Dashboard />} />
 
         {/* <Route path="/topic/:topicId" element={<TopicView />} /> */}
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 )
